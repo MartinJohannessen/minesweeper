bd01713113067fe0cabdec4c34d380beea8f7187
Martin Johannessen <martin.jodd@gmail.com>
2020-02-01 00:18:30 +0100

    added a class called minefield

--- Program.cs
+++ Program.cs
@@ -1,12 +1,57 @@
 ﻿using System;
+using System.Data.Entity.Core.Common.CommandTrees.ExpressionBuilder;
 
 namespace minesweeper
 {
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+
         }
     }
+
+
+
+    public class Minefield
+    {
+        private int[,] minefield;
+
+        public Minefield(int row, int col, int mines)
+        {
+        
+            minefield = new int[row, col];
+
+            Random rnd = new Random();
+
+
+            while (mines > 0)
+            {
+                int xbomb = rnd.Next(row);
+                int ybomb = rnd.Next(col);
+
+
+                int valueXY = minefield[xbomb, ybomb];
+                if (valueXY < 0) { continue; }
+
+                minefield[xbomb, ybomb] = -10;
+                mines--;
+            }
+        }
+        
+
+
+  
+
+    }
+
+
+
+
+
+
+
 }
